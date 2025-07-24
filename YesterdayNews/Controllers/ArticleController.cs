10b9b9fc19cd8d8d0cc1cccc4c4e2458f20a01fb
youssefHikal <youssefhikal@gmail.com>
2025-07-24 15:09:41 +0200

    Structure completed

--- YesterdayNews/Controllers/ArticleController.cs
+++ YesterdayNews/Controllers/ArticleController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using YesterdayNews.Services.IServices;
 
 namespace YesterdayNews.Controllers
 {
     public class ArticleController : Controller
     {
+        private readonly IArticleServices _articleServices;
+
+        public ArticleController(IArticleServices articleServices)
+        {
+            _articleServices = articleServices;
+        }
         public IActionResult Index()
         {
             return View();
--- YesterdayNews/Services/ArticleServices.cs
+++ YesterdayNews/Services/ArticleServices.cs
@@ -1,6 +1,22 @@
-﻿namespace YesterdayNews.Services
+﻿using YesterdayNews.Data;
+using YesterdayNews.Models.Db;
+using YesterdayNews.Services.IServices;
+
+namespace YesterdayNews.Services
 {
-    public class ArticleServices
+    public class ArticleServices : IArticleServices
     {
+        private readonly ApplicationDbContext _db;
+
+
+        public ArticleServices(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<Article> GetAll()
+        {
+            return _db.Articles.ToList();
+        }
     }
 }
