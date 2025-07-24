c5ef82469aaba94814f4b52e66cdf1f1fd2d77a9
anderikim <anders.eriksson.sum@gmail.com>
2025-07-24 15:48:50 +0200

    created index as GetAll

--- YesterdayNews/Services/SubscriptionServices.cs
+++ YesterdayNews/Services/SubscriptionServices.cs
@@ -1,6 +1,19 @@
-﻿namespace YesterdayNews.Services
+﻿using YesterdayNews.Data;
+using YesterdayNews.Models.Db;
+using YesterdayNews.Services.IServices;
+
+namespace YesterdayNews.Services
 {
-    public class SubscriptionServices
+    public class SubscriptionServices : ISubscriptionServices
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public SubscriptionServices (ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public List <Subscription> GetAll()
+        {
+            return _applicationDbContext.Subscriptions.ToList();
+        }
     }
 }
