283d68645b64421e68cc41ada0c7592d29cc8ab4
SamiElhag <samielhag1@outlook.com>
2025-07-24 09:48:08 +0200

    Description-deleted

--- YesterdayNews/Models/Category.cs
+++ YesterdayNews/Models/Category.cs
@@ -9,7 +9,6 @@ namespace YesterdayNews.Models
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
-        [StringLength(255)]
-        public string Description { get; set; }
+       
     }
 }
