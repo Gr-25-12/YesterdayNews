b9b91f5b7bf3a92f57e1a269c99306fe397b1d49
SamiElhag <samielhag1@outlook.com>
2025-07-24 09:23:22 +0200

    Category Model(Class + adde to Dbcontext)

---
+++ YesterdayNews/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YesterdayNews.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string Description { get; set; }
+    }
+}
