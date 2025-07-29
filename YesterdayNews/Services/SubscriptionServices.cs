b83aeef55fdde9b5242d27bedac45e885c51e771
anderikim <anders.eriksson.sum@gmail.com>
2025-07-29 14:49:52 +0200

    push

---
+++ YesterdayNews/Services/SubscriptionServices.cs
@@ -0,0 +1,19 @@
+﻿using YesterdayNews.Data;
+using YesterdayNews.Models.Db;
+using YesterdayNews.Services.IServices;
+
+namespace YesterdayNews.Services
+{
+    public class SubscriptionServices : ISubscriptionServices
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public SubscriptionServices (ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public List <Subscription> GetAll()
+        {
+            return _applicationDbContext.Subscriptions.ToList();
+        }
+    }
+}
---
+++ YesterdayNews/Services/SubscriptionTypeServices.cs
@@ -0,0 +1,26 @@
+﻿using YesterdayNews.Data;
+using YesterdayNews.Models.Db;
+using YesterdayNews.Services.IServices;
+
+namespace YesterdayNews.Services
+{
+    public class SubscriptionTypeServices : ISubscriptionTypeServices
+    {
+        private readonly ApplicationDbContext _db;
+        public SubscriptionTypeServices(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public List<SubscriptionType> GetAll()
+        {
+            return _db.SubscriptionTypes.ToList();
+        }
+
+        public SubscriptionType GetOne(int id)
+        {
+            var SubscriptionList = _db.SubscriptionTypes.FirstOrDefault(m => m.Id == id);
+            return SubscriptionList;
+        }
+
+    }
+}
