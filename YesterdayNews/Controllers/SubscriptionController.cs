cb8c45da992411e2d99710e175303751404784f3
Chatzo <chatzoglakis@gmail.com>
2025-07-31 16:41:11 +0200

    Added basic SubscriptionController and a basic Create_view for subscriptions
    (commit for end of day)

---
+++ YesterdayNews/Controllers/SubscriptionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using YesterdayNews.Models.Db;
+using YesterdayNews.Services;
+using YesterdayNews.Services.IServices;
+
+namespace YesterdayNews.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        private readonly ISubscriptionServices _subscriptionServices;
+        private readonly ISubscriptionTypeServices _subscriptionTypeServices;
+        public SubscriptionController(ISubscriptionServices subscriptionServices, ISubscriptionTypeServices subscriptionTypeServices) 
+        {
+            _subscriptionServices = subscriptionServices;
+            _subscriptionTypeServices = subscriptionTypeServices;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var types = _subscriptionTypeServices.GetAll();
+            ViewBag.SubscriptionTypeId = new SelectList(types, "Id", "Name");
+            return View();
+        }
+    }
+}
