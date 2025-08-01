29d6be67a29aaeed76e0faab9f544a94ad37c696
youssefHikal <youssefhikal@gmail.com>
2025-08-01 15:22:43 +0200

    Refacroted delete sweet alret

--- YesterdayNews/wwwroot/js/Article.js
+++ YesterdayNews/wwwroot/js/Article.js
@@ -77,32 +77,32 @@ function loadDataTable(status) {
         ],
     });
 }
-function Delete(url) {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: url,
-                type: 'DELETE',
-                success: function (data) {
-                    if (data.success) {
-                        dataTable.ajax.reload();
-                        toastr.success(data.message);
-                    } else {
-                        toastr.error(data.message);
-                    }
-                }
-            });
-        }
-    });
-}
+//function Delete(url) {
+//    Swal.fire({
+//        title: 'Are you sure?',
+//        text: "You won't be able to revert this!",
+//        icon: 'warning',
+//        showCancelButton: true,
+//        confirmButtonColor: '#3085d6',
+//        cancelButtonColor: '#d33',
+//        confirmButtonText: 'Yes, delete it!'
+//    }).then((result) => {
+//        if (result.isConfirmed) {
+//            $.ajax({
+//                url: url,
+//                type: 'DELETE',
+//                success: function (data) {
+//                    if (data.success) {
+//                        dataTable.ajax.reload();
+//                        toastr.success(data.message);
+//                    } else {
+//                        toastr.error(data.message);
+//                    }
+//                }
+//            });
+//        }
+//    });
+//}
 function setupImagePreview() {
     const input = document.getElementById('imageInput');
     const preview = document.getElementById('imagePreview');
--- YesterdayNews/wwwroot/js/subscriptionType.js
+++ YesterdayNews/wwwroot/js/subscriptionType.js
@@ -2,6 +2,12 @@
 
 function loadDataTable() {
     dataTable = $('#myDataTable').DataTable({
+        "language": {
+            "searchPlaceholder": "Search Subscriptions types...",
+        },
+        "responsive": true,
+        "processing": true,
+        "serverSide": false,
         "ajax": {
             url: '/SubscriptionType/GetAll',
             type: 'GET',
@@ -45,35 +51,6 @@ function loadDataTable() {
     });
 }
 
-function Delete(url) {
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: url,
-                type: 'DELETE',
-                success: function (data) {
-                    if (data.success) {
-                        dataTable.ajax.reload();
-                        toastr.success(data.message);
-                    } else {
-                        toastr.error(data.message);
-                    }
-                },
-                error: function () {
-                    toastr.error("An error occurred while deleting");
-                }
-            });
-        }
-    });
-}
 
 $(document).ready(function () {
     loadDataTable();
