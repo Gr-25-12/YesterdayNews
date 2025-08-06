111b87b91c04f0bd23eafcc8b944c7ec09891e94
Chatzo <chatzoglakis@gmail.com>
2025-08-06 13:55:17 +0200

    fixed a bug when editing an article making image to be deleted.

--- YesterdayNews/Controllers/ArticleController.cs
+++ YesterdayNews/Controllers/ArticleController.cs
@@ -208,7 +208,11 @@ namespace YesterdayNews.Controllers
             existing.AuthorId = article.AuthorId;
             string newImageLink = await UploadImage(article, imageFile);
             if (newImageLink != null)
+            {
+                _fileServices.DeleteFileFromContainer(existing.ImageLink);
                 existing.ImageLink = newImageLink;
+            }
+                
 
             if (article.ArticleStatus == ArticleStatus.Published)
                 existing.ArticleStatus = ArticleStatus.PendingReview;
@@ -222,7 +226,6 @@ namespace YesterdayNews.Controllers
             }
             // Save to DB
             _articleServices.Edit(existing);
-            _fileServices.DeleteFileFromContainer(article.ImageLink);
             TempData["success"] = "Article updated successfully!";
             return RedirectToAction("Index");
         }
