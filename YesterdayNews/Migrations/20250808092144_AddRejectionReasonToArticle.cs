690193074520ec879216ac7ec199c89c3cfd2fc3
SamiElhag <samielhag1@outlook.com>
2025-08-08 14:54:45 +0200

    Displaying the Subscription tab in the Identity area.
    Highlighting the active page using the ManageNavPages logic.
    
    Seeing the expected output: "You are not currently subscribed."
    
    And confirming the route works: Current page: /Account/Manage/Subscription.

---
+++ YesterdayNews/Migrations/20250808092144_AddRejectionReasonToArticle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YesterdayNews.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRejectionReasonToArticle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
