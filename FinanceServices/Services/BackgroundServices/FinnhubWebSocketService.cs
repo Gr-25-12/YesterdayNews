b1184a25c8468f631367c192e7ec4dce94dfa604
Chatzo <chatzoglakis@gmail.com>
2025-08-21 16:38:26 +0200

    added ApiCalls counter. also refactoring
    splitting FihnubBackgroundService into FinnhubWebsocketService and FinnhubApiService

---
+++ FinanceServices/Services/BackgroundServices/FinnhubWebSocketService.cs
@@ -0,0 +1,110 @@
+﻿using FinanceServices.Data;
+using FinanceServices.Models.API;
+using FinanceServices.Utilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+
+namespace FinanceServices.Services.BackgroundServices
+{
+    public class FinnhubWebSocketService : BackgroundService
+    {
+
+        public event Action<Dictionary<string, object>>? OnPriceUpdate;
+        private readonly string _apiKey;
+        private readonly MarketDataCache _cache;
+
+        public FinnhubWebSocketService(IConfiguration config, MarketDataCache Cache)
+        {
+            _apiKey = "" + config["Finnhub:ApiKey"];
+            _cache = Cache;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var symbolsList = FinanceConstants.LargeSymbolsList;
+
+            //Connect
+            using var websocket = new ClientWebSocket(); //"using" keyword ensure its disposed automatically when out of scope
+            var url = $"wss://ws.finnhub.io?token={_apiKey}";
+            await websocket.ConnectAsync(new Uri(url), stoppingToken);
+
+            //Subscribe-Send
+            foreach (var sym in symbolsList)
+            {
+                var msg = JsonSerializer.Serialize(new { type = "subscribe", symbol = sym });
+                await websocket.SendAsync(Encoding.UTF8.GetBytes(msg), WebSocketMessageType.Text, true, stoppingToken);
+            }
+
+            var buffer = new byte[4096];
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //Recieve
+                var result = await websocket.ReceiveAsync(new ArraySegment<byte>(buffer), stoppingToken);
+                if (result.MessageType == WebSocketMessageType.Close)
+                    break;
+
+                var json = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                try
+                {
+                    var response = JsonSerializer.Deserialize<TradesResponse>(json);
+                    if (response?.Data != null && response.Data.Count > 0)
+                    {
+                        //store updated prices in cached StockQuotes and CryptoQuotes
+                        foreach (var trade in response.Data)
+                        {
+                            if (_cache.StockQuotes.ContainsKey(trade.Symbol))
+                            {
+                                var stock = _cache.StockQuotes[trade.Symbol];
+                                stock.CurrentPrice = trade.Price;
+                                stock.TimeStamp = trade.TimeStamp;
+                            }
+                            else if (_cache.CryptoQuotes.ContainsKey(trade.Symbol))
+                            {
+                                var crypto = _cache.CryptoQuotes[trade.Symbol];
+                                crypto.CurrentPrice = trade.Price;
+                                crypto.TimeStamp = trade.TimeStamp;
+                            }
+                        }
+                        var updates = MergeStocksAndCryptos();
+                        RaisePriceUpdate(updates);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex}, WebSocket parse error");
+                }
+            }
+        }
+        private void RaisePriceUpdate(Dictionary<string, object> updates)
+        {
+            OnPriceUpdate?.Invoke(updates);
+        }
+        private Dictionary<string, object> MergeStocksAndCryptos()
+        {
+            var updates = new Dictionary<string, object>();
+            foreach (var stockQuote in _cache.StockQuotes)
+            {
+                updates[stockQuote.Key] = new
+                {
+                    stockQuote.Value.CurrentPrice,
+                    stockQuote.Value.Change,
+                    stockQuote.Value.PercentageChange,
+                };
+            }
+            foreach (var crypto in _cache.CryptoQuotes)
+            {
+                updates[crypto.Key] = new
+                {
+                    crypto.Value.CurrentPrice,
+                    crypto.Value.Change,
+                    crypto.Value.PercentageChange,
+                };
+            }
+            return updates;
+        }
+    }
+}
---
+++ FinanceServices/Utilities/FinnhubApiCallsCounter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceServices.Utilities
+{
+    public class FinnhubApiCallsCounter
+    {
+        private static int SecondCounter { get; set; } = 0;
+        private static int MinuteCounter { get; set; } = 0;
+        private static int DayCounter { get; set; } = 0;
+        private const int MAX_CALL_PERSECOND = 30;
+        private const int MAX_CALL_PERMINUTE = 60;
+        private const int MAX_CALL_PERDAY = 500;
+
+        private static DateTime lastSecondReset = DateTime.UtcNow;
+        private static DateTime lastMinuteReset = DateTime.UtcNow;
+        private static DateTime lastDayReset = DateTime.UtcNow;
+
+        private readonly object _lock = new();
+        public bool IsCallPossible()
+        {
+            lock (_lock)
+            {
+                var now = DateTime.UtcNow;
+
+                // Reset counters if interval passed
+                if ((now - lastSecondReset).TotalSeconds >= 1)
+                    ResetSecondCounter(now);
+                if ((now - lastMinuteReset).TotalMinutes >= 1)
+                    ResetMinuteCounter(now);
+                if ((now - lastDayReset).TotalDays >= 1)
+                    ResetDayCounter(now);
+
+                if (IsLimitReached())
+                {
+                    return false;
+                }
+                IncrementCounters();
+                PrintToConsole();
+                return true;
+            }
+        }
+        private static void ResetSecondCounter(DateTime now)
+        {
+            SecondCounter = 0;
+            lastSecondReset = now;
+        }
+        private static void ResetMinuteCounter(DateTime now)
+        {
+            MinuteCounter = 0;
+            lastMinuteReset = now;
+        }
+        private static void ResetDayCounter(DateTime now)
+        {
+            DayCounter = 0;
+            lastDayReset = now;
+        }
+
+        private static bool IsLimitReached()
+        {
+            if (SecondCounter >= MAX_CALL_PERSECOND ||
+                    MinuteCounter >= MAX_CALL_PERMINUTE ||
+                    DayCounter >= MAX_CALL_PERDAY)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static void IncrementCounters()
+        {
+            SecondCounter++;
+            MinuteCounter++;
+            DayCounter++;
+        }
+
+        /// <summary>
+        /// Use this for testing only
+        /// </summary>
+        public void PrintToConsole()
+        {
+            Console.WriteLine($"Second counter: {SecondCounter}");
+            Console.WriteLine($"Minute counter: {MinuteCounter}");
+            Console.WriteLine($"Day counter: {DayCounter}");
+        }
+    }
+}
