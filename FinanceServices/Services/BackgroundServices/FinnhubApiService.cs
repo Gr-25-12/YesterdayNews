9b4c662121724834de77a40bf1bee279a6a30ab8
Chatzo <chatzoglakis@gmail.com>
2025-08-29 14:43:46 +0200

    fixed errors after merge

--- FinanceServices/Services/BackgroundServices/FinnhubApiService.cs
+++ FinanceServices/Services/BackgroundServices/FinnhubApiService.cs
@@ -56,10 +56,6 @@ namespace FinanceServices.Services.BackgroundServices
                 }
                 await Task.Delay(TimeSpan.FromMinutes(30), stoppingToken);
             }
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                await UpdateMarketStatus(stoppingToken);
-            }
         }
 
         private async Task RunUpdateListsLoop(CancellationToken stoppingToken)
